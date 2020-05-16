010067b15cec280c4ddcf31b4fcc9be96575a9f0
jesinmat <jesinmat@fit.cvut.cz>
2020-05-16 16:47:31 +0200

    Add command line arguments for script

--- games/mc/game_properties.sh
+++ games/mc/game_properties.sh
@@ -1,6 +1,2 @@
 GAME="mc"
 GAME_LONG_NAME="Minecraft (Java Edition)"
-
-# Gamedig
-GAMEDIG_TYPE="minecraft"
-GAMEDIG_PORT="25565"
--- games/q3/game_properties.sh
+++ games/q3/game_properties.sh
@@ -1,6 +1,2 @@
 GAME="q3"
 GAME_LONG_NAME="Quake III Arena"
-
-# Gamedig
-GAMEDIG_TYPE="quake3"
-GAMEDIG_PORT="27960"
