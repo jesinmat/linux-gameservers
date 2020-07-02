b579a02d1f0984c44dd38fe308d3d448afd5e34f
jesinmat <jesinmat@fit.cvut.cz>
2020-07-02 17:04:11 +0200

    Add config for Minecraft

--- games/mc/post_install.sh
+++ games/mc/post_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
 # Get current IP and remove trailing space
 IP=$(hostname -I | xargs)
-export IP
 run_as_user 'sed -i "s/server-ip=/server-ip='"$IP"'/" '"$GAMEDIR"'/serverfiles/server.properties'
+
+# Set server admin (OP)
+if [ -z "$GAME_ADMIN" ]; then
+    read -p "Server admin username (your username): " GAME_ADMIN || GAME_ADMIN=''
+    echo
+fi
+run_as_user 'echo '"$GAME_ADMIN"' > '"$GAMEDIR"'/serverfiles/ops.txt'
