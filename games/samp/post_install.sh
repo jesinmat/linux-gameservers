9248d8385aed355f386804d8d7b681ef7d05b845
jesinmat <jesinmat@fit.cvut.cz>
2020-07-02 15:17:53 +0200

    Fix typo

--- games/samp/post_install.sh
+++ games/samp/post_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ -z "$GAME_RCON_PASS" ]; then
-    read -ps "Set RCON password:" GAME_RCON_PASS || GAME_RCON_PASS=`mcookie`
+    read -s -p "Set RCON password:" GAME_RCON_PASS || GAME_RCON_PASS=`mcookie`
 fi
 run_as_user 'sed -i "s/rcon_password changeme/rcon_password '"$GAME_RCON_PASS"'/" '"$GAMEDIR"'/serverfiles/samp03/server.cfg'
