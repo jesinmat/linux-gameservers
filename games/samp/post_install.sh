9761b0a5346a4457dc3d245b423082f857eb9de5
jesinmat <jesinmat@fit.cvut.cz>
2020-07-02 14:53:12 +0200

    Use env variable for SA:MP

--- games/samp/post_install.sh
+++ games/samp/post_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# TODO random password
-PASS="randompass"
-[ -n "$APP_PASS" ] && PASS="$APP_PASS"
-run_as_user 'sed -i "s/rcon_password changeme/rcon_password '"$PASS"'/" '"$GAMEDIR"'/serverfiles/samp03/server.cfg'
+if [ -z "$GAME_RCON_PASS" ]; then
+    read -ps "Set RCON password:" GAME_RCON_PASS || GAME_RCON_PASS=`mcookie`
+fi
+run_as_user 'sed -i "s/rcon_password changeme/rcon_password '"$GAME_RCON_PASS"'/" '"$GAMEDIR"'/serverfiles/samp03/server.cfg'
